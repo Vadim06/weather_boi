46e99ae85e9989b50299770ba02ab1ad7782354f
Vadym <vadim.mikhaylets@gmail.com>
2020-11-14 17:45:43 +0100

    template preparation

---
+++ src/components/First.spec.tsx
@@ -0,0 +1,24 @@
+import First from './First';
+
+describe('First', () => {
+  it('simple test', () => {
+    expect(0).toBe(0);
+  });
+
+  it('shold be jasmine to global namespace', () => {
+    const spy = jasmine.createSpy('');
+    spy('first arg', 'second arg');
+    expect(spy.calls.allArgs()).toEqual([
+      ['first arg', 'second arg']
+    ]);
+  });
+
+  it('real test', () => {
+    const instance = new First();
+    const spy = jasmine.createSpy('');
+    instance.makeErrorToConsoleLog.call({
+      props: { error: spy }
+    });
+    expect(spy.calls.count()).toBe(1);
+  });
+});
---
+++ src/components/First.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+
+export default class First extends React.Component<any, any> {
+  render() {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h1 style={{ width: '50%', margin: '0 auto' }}>
+          Hello! I’m JSX
+        </h1>
+
+        <button onClick={this.makeErrorToConsoleLog}>
+          Click me for error to console.log (test of source-map)
+        </button>
+      </div>
+    );
+  }
+
+  makeErrorToConsoleLog() {
+    // test for source-map
+    this.props.error();
+  }
+}
