ee31dc98b93421e6b7cc51bce95401d20f382469
Vadym <vadim.mikhaylets@gmail.com>
2020-11-17 21:27:59 +0100

    Material UI

--- src/components/First.tsx
+++ src/components/First.tsx
@@ -1,27 +1,37 @@
 import * as React from 'react';
-
-
+import { Checkbox } from '@material-ui/core';
+import Badge from '@material-ui/core/Badge';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import { TextField } from '@material-ui/core';
 
 const First: React.FC = () => {
-const [isChecked, setIsChecked] = React.useState<boolean>(false);
+  const [isChecked, setIsChecked] = React.useState<boolean>(false);
+  const inputEl = React.useRef<HTMLInputElement>(null);
   const changeHandler = () => {
     console.log("checked");
     setIsChecked(!isChecked);
-
   }
-return (
-<div style={{ textAlign: 'center' }}>
-  <h1 style={{ width: '50%', margin: '0 auto' }}>
-    Hello! I’m JSX
+  const changeInputHandler = React.useCallback(() => {
+    if (inputEl && inputEl.current)
+    console.log(inputEl.current.value)
+  }, [inputEl])
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1 style={{ width: '50%', margin: '0 auto' }}>
+        Hello! I’m JSX
   </h1>
-  <div>
-    {isChecked && <div>Got it</div>}
-  </div>
-  <div>
-    <input onChange={changeHandler} type="checkbox" id="vehicle" name="vehicle" value="Boat"/>
-  </div>
-</div>
-);
+      <div>
+        <Checkbox onChange={changeHandler} />
+        <div>
+          <Badge badgeContent={isChecked ? 1 : 0} color="primary">
+            <SubscriptionsIcon />
+          </Badge>
+          <br />
+          <TextField placeholder="Type here" variant="outlined" label="City" inputRef={inputEl} onChange={changeInputHandler} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default First;
