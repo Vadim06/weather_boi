8dff7eea2c1412761dbd02761113848f3af0a6bf
Vadym <vadim.mikhaylets@gmail.com>
2020-11-23 21:42:45 +0100

    make styles

--- src/components/First.tsx
+++ src/components/First.tsx
@@ -3,9 +3,26 @@ import { Checkbox } from '@material-ui/core';
 import Badge from '@material-ui/core/Badge';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
 import { TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  checkbox: {
+    margin: '20px 10px 15px',
+    padding: '30px',
+    border: '2px dotted pink',
+
+  }
+})
 
 const First: React.FC = () => {
   const [isChecked, setIsChecked] = React.useState<boolean>(false);
+
+  // let isChecked: boolean = false;
+
+  // function setIsChecked(znachenie) {
+  //   isChecked = znachenie;
+  // }
+
   const inputEl = React.useRef<HTMLInputElement>(null);
   const changeHandler = () => {
     console.log("checked");
@@ -13,15 +30,18 @@ const First: React.FC = () => {
   }
   const changeInputHandler = React.useCallback(() => {
     if (inputEl && inputEl.current)
-    console.log(inputEl.current.value)
+      console.log(inputEl.current.value)
   }, [inputEl])
+
+  const classes = useStyles();
+
   return (
     <div style={{ textAlign: 'center' }}>
       <h1 style={{ width: '50%', margin: '0 auto' }}>
         Hello! I’m JSX
   </h1>
       <div>
-        <Checkbox onChange={changeHandler} />
+        <Checkbox className={classes.checkbox} onChange={changeHandler} />
         <div>
           <Badge badgeContent={isChecked ? 1 : 0} color="primary">
             <SubscriptionsIcon />
