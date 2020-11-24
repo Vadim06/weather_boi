a17356b4a1aa8c15544650886e80c7e6caddceda
Volodymyr Mykhailets <volodymyrm@oriflame.com>
2020-11-24 20:55:06 +0100

    play with checkbox

--- src/components/First.tsx
+++ src/components/First.tsx
@@ -1,21 +1,35 @@
-import * as React from 'react';
-import { Checkbox } from '@material-ui/core';
-import Badge from '@material-ui/core/Badge';
-import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
-import { TextField } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import * as React from "react";
+import { Checkbox } from "@material-ui/core";
+import Badge from "@material-ui/core/Badge";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import { TextField } from "@material-ui/core";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+    },
 
-const useStyles = makeStyles({
-  checkbox: {
-    display: 'flex',
-    marginLeft: '709px',
-  },
-  flexContainer: {
-    display: 'flex',
-    alignItems: 'left',
-    justifyContent: 'left',
-  },
-});
+    checkbox: {
+      display: "flex",
+      position: "absolute",
+      left: '50%',
+      transform: 'translateX(-50%)',
+    },
+    flexContainer: {
+      display: "flex",
+      position: "relative",
+      justifyContent: "left",
+    },
+  })
+);
 
 const First: React.FC = () => {
   const [isChecked, setIsChecked] = React.useState<boolean>(false);
@@ -30,31 +44,59 @@ const First: React.FC = () => {
   const changeHandler = () => {
     console.log("checked");
     setIsChecked(!isChecked);
-  }
+  };
   const changeInputHandler = React.useCallback(() => {
-    if (inputEl && inputEl.current)
-      console.log(inputEl.current.value)
-  }, [inputEl])
+    if (inputEl && inputEl.current) console.log(inputEl.current.value);
+  }, [inputEl]);
 
   const classes = useStyles();
 
   return (
-    <div style={{ textAlign: 'center' }}>
-      <div className={classes.flexContainer}>
-        <TextField  placeholder="Type here" variant="outlined" label="City" inputRef={inputEl} onChange={changeInputHandler} />
-        <Checkbox className={classes.checkbox} onChange={changeHandler} />
-      </div>
-      <div>
+    <>
+      <div style={{ textAlign: "center" }}>
+        <div className={classes.flexContainer}>
+          <TextField
+            placeholder="Type here"
+            variant="outlined"
+            label="City"
+            inputRef={inputEl}
+            onChange={changeInputHandler}
+          />
+          <Checkbox className={classes.checkbox} onChange={changeHandler} />
+        </div>
         <div>
-          <Badge badgeContent={isChecked ? 1 : 0} color="primary">
-            <SubscriptionsIcon />
-          </Badge>
-          <br />
-
+          <div>
+            <Badge badgeContent={isChecked ? 1 : 0} color="primary">
+              <SubscriptionsIcon />
+            </Badge>
+            <br />
+          </div>
         </div>
       </div>
-    </div>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={3} lg={2}>
+            <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3} lg={2}>
+            <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3} lg={2}>
+            <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3} lg={2}>
+            <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3} lg={2}>
+            <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3} lg={2}>
+            <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </>
   );
-}
+};
 
 export default First;
